2aee90f83a28cd3a3495874cf3482a1e0bdb0cbb
Robin Fröjd <robinae@hotmail.se>
2019-06-14 11:35:19 +0200

    first test
    connection successful

---
+++ script.py
@@ -0,0 +1,32 @@
+import time
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('185.112.115.234', 5000))
+client_socket.settimeout(10)
+
+time.sleep(2)
+string = "status 1" + '\r\n'
+arr = bytes(string, 'utf-8')
+client_socket.send(arr)
+
+GPS_List = [0.0]
+
+while True:
+
+	data = client_socket.recv(1024)
+	if data == "":
+		break
+	GPS_List = data.split()
+
+	print (GPS_List[1])
+	#Solution = GPS_List[40]
+	#print (Solution)
+	#x = 1
+	#for str in GPS_List:
+	#	print ("%s %s") % (x, str)
+	#	x += 1
+
+
+
+	time.sleep(1)
